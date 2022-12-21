2901233917568e1516de2ab2b9c2515f3c50d45f
Cupu-Boy <fajrir357@gmail.com>
2022-12-21 23:41:09 +0700

    update contact controller

--- src/controller/contacts.controller.js
+++ src/controller/contacts.controller.js
@@ -42,7 +42,7 @@ exports.readContact = (req, res) => {
 };
 
 exports.createContact = (req, res) => {
-  req.boyd.userId = req.userData.id
+  req.body.userId = req.userData.id
   insertContact(req.body, (err, data) => {
     if (err) {
       console.log(err);
